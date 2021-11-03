d22d907fd67c4a29f19b737b5a611bc380b618f1
AconLight <w.mosienski@gmail.com>
2021-11-03 22:00:02 +0100

    part hero right left

--- Assets/scripts/partPicker.cs
+++ Assets/scripts/partPicker.cs
@@ -4,7 +4,22 @@ using UnityEngine;
 
 public class partPicker : MonoBehaviour
 {
+    public List<GameObject> availableParts;
+
+    public int currentPart = 0;
     // Start is called before the first frame update
+
+    public void moveRight() {
+        currentPart += 1;
+        currentPart %= availableParts.Count;
+    }
+
+    public void moveLeft() {
+        currentPart -= 1;
+        currentPart %= availableParts.Count;
+    }
+
+
     void Start()
     {
         
